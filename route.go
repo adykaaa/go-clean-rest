dcba3d5d949b6b7c7b8c8bebd4c9fd7bfa8c76f5
adykaaa <gosupalanta@gmail.com>
2022-11-20 13:44:14 +0100

    initial commit

---
+++ route.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Post struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+var (
+	posts []Post
+)
+
+func init() {
+	posts = []Post{{Id: 1, Title: "Title 1", Text: "Text 1"}}
+}
+
+func getPosts(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
+	result, err := json.Marshal(posts)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error marshaling the Posts array"}`))
+		return
+	}
+	resp.WriteHeader(http.StatusOK)
+	resp.Write(result)
+	fmt.Println("GET /posts hit!")
+
+}
+
+func addPost(resp http.ResponseWriter, req *http.Request) {
+	var post Post
+	resp.Header().Set("Content-type", "application/json")
+	err := json.NewDecoder(req.Body).Decode(&post)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error marshaling the Posts array"}`))
+		return
+	}
+	post.Id = len(posts) + 1
+	posts = append(posts, post)
+	resp.WriteHeader(http.StatusOK)
+	result, err := json.Marshal(post)
+	resp.Write(result)
+}
