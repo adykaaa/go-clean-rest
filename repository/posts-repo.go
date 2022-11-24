625733baa39de6ec15a3354f8cd07ac08176004c
adykaaa <gosupalanta@gmail.com>
2022-11-24 08:46:43 +0100

    starting to implement CA

--- repository/posts-repo.go
+++ repository/posts-repo.go
@@ -1,80 +1,10 @@
 package repository
 
 import (
-	"context"
-	"log"
-
-	"cloud.google.com/go/firestore"
 	"github.com/adykaaa/go-clean-rest/entity"
-	"google.golang.org/api/iterator"
 )
 
 type PostRepository interface {
 	Save(post *entity.Post) (*entity.Post, error)
 	FindAll() ([]entity.Post, error)
-}
-
-type repo struct {
-}
-
-func NewPostRepository() PostRepository {
-	return repo{}
-}
-
-const (
-	projectId      string = "pragmatic-reviews-428f7"
-	collectionName string = "posts"
-)
-
-func (repo) Save(post *entity.Post) (*entity.Post, error) {
-	ctx := context.Background()
-	client, err := firestore.NewClient(ctx, projectId)
-	if err != nil {
-		log.Fatalf("Failed to create a Firestore Client: %v", err)
-		return nil, err
-	}
-
-	defer client.Close()
-
-	_, _, err = client.Collection(collectionName).Add(ctx, map[string]interface{}{
-		"ID":    post.ID,
-		"Title": post.Title,
-		"Text":  post.Text,
-	})
-	if err != nil {
-		log.Fatalf("Failed adding a new post: %v", err)
-	}
-
-	return post, nil
-}
-
-func (repo) FindAll() ([]entity.Post, error) {
-	ctx := context.Background()
-	client, err := firestore.NewClient(ctx, projectId)
-	if err != nil {
-		log.Fatalf("Failed to create a Firestore Client: %v", err)
-		return nil, err
-	}
-	defer client.Close()
-
-	var posts []entity.Post
-	iter := client.Collection(collectionName).Documents(ctx)
-	for {
-		doc, err := iter.Next()
-		if err != nil {
-			if err == iterator.Done {
-				break
-			}
-			log.Fatalf("Failed to iterate the list of posts: %v", err)
-			return nil, err
-		}
-		post := entity.Post{
-			ID:    doc.Data()["ID"].(int64),
-			Title: doc.Data()["Title"].(string),
-			Text:  doc.Data()["Text"].(string),
-		}
-
-		posts = append(posts, post)
-	}
-	return posts, nil
 }
---
+++ service/post-service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"errors"
+	"math/rand"
+
+	"github.com/adykaaa/go-clean-rest/entity"
+	"github.com/adykaaa/go-clean-rest/repository"
+)
+
+type PostService interface {
+	Validate(post *entity.Post) error
+	Create(post *entity.Post) (*entity.Post, error)
+	FindAll() ([]entity.Post, error)
+}
+
+type service struct{}
+
+var (
+	repo repository.PostRepository = repository.NewFirestoreRepository()
+)
+
+func NewPostService() PostService {
+	return &service{}
+}
+
+func (*service) Validate(post *entity.Post) error {
+	if post == nil {
+		return errors.New("The post is nil!")
+	}
+
+	if post.Title == "" {
+		return errors.New("The post title is empty!")
+	}
+	return nil
+}
+
+func (*service) Create(post *entity.Post) (*entity.Post, error) {
+	post.ID = rand.Int63()
+	return repo.Save(post)
+}
+
+func (*service) FindAll() ([]entity.Post, error) {
+	return repo.FindAll()
+}
